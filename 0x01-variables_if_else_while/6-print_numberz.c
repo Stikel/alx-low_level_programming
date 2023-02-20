fe01a759e132f63ddc5658d1e15e0f19469e6445
Alex <alex_ores@yahoo.com>
2023-02-20 15:25:01 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Suceess)
+ */
+
+int main(void)
+{
+	putchar(48); /* ASCII value of '0' */
+	putchar(49); /* ASCII value of '1' */
+	putchar(50); /* ASCII value of '2' */
+	putchar(51); /* ASCII value of '3' */
+	putchar(52); /* ASCII value of '4' */
+	putchar(53); /* ASCII value of '5' */
+	putchar(54); /* ASCII value of '6' */
+	putchar(55); /* ASCII value of '7' */
+	putchar(56); /* ASCII value of '8' */
+	putchar(57); /* ASCII value of '9' */
+	putchar(''\n);
+
+	return (0);
+}
