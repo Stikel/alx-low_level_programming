82766515abfe39883081039e54a62f9f511eba70
Alex <alex_ores@yahoo.com>
2023-02-20 10:27:06 -0800

    101-quote.c:5: warning: missing initial short description

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,12 @@
 /**
  * stdio libery for input and output
  *
- * string library for char and puts function
- *
- * main -  This program prints string using write function
+ * main - Entry point
  * Return 1 (Success)
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	return: (1);
 }
