c027395ada7cad08f9e05366247846f86612bbba
Alex <alex_ores@yahoo.com>
2023-02-20 13:47:19 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	char alp[26] = "abcdefghijklmnopqrstvwzyz";
+{
+	char alp[26] - "abcdefghijklmnopqrstvwzyz";
 	int i;
-
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
-}  
-
+}
