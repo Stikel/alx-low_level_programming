2b10e12f50fe7bfff5d4516cabcb51fe18022799
Alex <alex_ores@yahoo.com>
2023-02-20 15:50:09 -0800

    7-print_tebahpla.c:10: WARNING: Missing a blank line after declarations

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 25; i >= 0; i--)
+	{
+	putchar(i + 'a');
+	}
+	putchar('\n');
+	return (0);
+}
