8679dc058c97e07a624f86f9fcae5232ac2a3f89
Alex <alex_ores@yahoo.com>
2023-02-20 15:34:14 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,16 +6,14 @@
 
 int main(void)
 {
-	putchar(48); /* ASCII value of '0' */
-	putchar(49); /* ASCII value of '1' */
-	putchar(50); /* ASCII value of '2' */
-	putchar(51); /* ASCII value of '3' */
-	putchar(52); /* ASCII value of '4' */
-	putchar(53); /* ASCII value of '5' */
-	putchar(54); /* ASCII value of '6' */
-	putchar(55); /* ASCII value of '7' */
-	putchar(56); /* ASCII value of '8' */
-	putchar(57); /* ASCII value of '9' */
+	int num = 0;
+
+	while (num < 10)
+		(
+		putchar(num + '0')
+		num++
+		)
+
 	putchar('\n');
 
 	return (0);
