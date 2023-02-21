855f6f4d640a1f0d736de387a254420d8a7214b5
Alex <alex_ores@yahoo.com>
2023-02-20 16:30:37 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,24 +6,19 @@
 
 int main(void)
 {
-	int i, j;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while
+		(i = 10);
 	{
-		for (j = i; j < 10; j++)
-		{
-			if (i != j)
+		putchar(i + '0')
+			if (i < 9)
 			{
-				putchar(i + '0');
-				putchar(j + '0');
-				if (i != 8 || j != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
-		}
 	}
+
 	putchar('\n');
 	return (0);
 }
