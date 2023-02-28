9f1a01d0503bea51761cee05a3100ca303138696
Alex <alex_ores@yahoo.com>
2023-02-28 03:44:51 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @S: string
+ * Return: 0
+ */
+void print_rev(cahr *S)
+{
+
+	int longi = 0;
+	int o;
+
+	while (*S != '\0')
+	{
+		longi++;
+		S++;
+	}
+	S--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*S);
+		S--;
+	}
+	_putchat('\n');
+}
