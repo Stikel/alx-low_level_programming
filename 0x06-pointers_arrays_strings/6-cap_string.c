7ef682cfdf9d5cd9402629944fd60baf4564db72
Alex <alex_ores@yahoo.com>
2023-03-02 11:59:36 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,8 +17,8 @@ char *cap_string(char *str)
 		str[index - 1] == '\t' ||
 		str[index - 1] == '\n' ||
 		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
 		str[index - 1] == '.' ||
-		str[index - 1] == '_' ||
 		str[index - 1] == '!' ||
 		str[index - 1] == '?' ||
 		str[index - 1] == '"' ||
